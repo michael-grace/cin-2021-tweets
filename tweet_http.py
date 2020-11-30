8f865be04d9fbb3441ec40a9b3014049e0f65157
michael-grace <michael.dp.grace@outlook.com>
2020-11-30 19:54:24 +0000

    Formatting

--- tweet_http.py
+++ tweet_http.py
@@ -14,21 +14,26 @@ import config
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def base():
     return "", 400
+
 
 @app.route("/board")
 def board():
     return redirect(url_for("static", filename="board.html"))
 
+
 @app.route("/graphic")
 def graphic():
     return redirect(url_for("static", filename="graphic.html"))
 
+
 @app.route("/control")
 def control():
     return redirect(url_for("static", filename="control.html"))
+
 
 @app.route("/info")
 def hashtag():
@@ -37,9 +42,11 @@ def hashtag():
         "ws_conn": "ws://{0}:{1}".format(config.HOST, config.WS_PORT)
     }
 
+
 def http_server() -> None:
     print("Starting HTTP Server")
     app.run(host=config.HOST, port=config.PORT, debug=True)
+
 
 if __name__ == "__main__":
     print("Don't do this")
--- tweet_ws.py
+++ tweet_ws.py
@@ -18,18 +18,22 @@ from typing import Dict
 
 pending_tweets: Dict[int, Dict[str, any]] = {}
 
+
 async def recv_tweets(websocket):
     api = Api(
-    config.TWITTER_API_KEY,
-    config.TWITTER_API_SECRET_KEY,
-    config.TWITTER_ACCESS_TOKEN,
-    config.TWITTER_ACCESS_TOKEN_SECRET,
-    tweet_mode="extended"
+        config.TWITTER_API_KEY,
+        config.TWITTER_API_SECRET_KEY,
+        config.TWITTER_ACCESS_TOKEN,
+        config.TWITTER_ACCESS_TOKEN_SECRET,
+        tweet_mode="extended"
     )
     for tweet in api.GetStreamFilter(track=[config.HASHTAG], languages=["en"]):
         if tweet["text"][:2] == "RT":
             if "extended_tweet" in tweet["retweeted_status"].keys():
-                body = "{0}: {1}".format(tweet["text"].split(":")[0], tweet["retweeted_status"]["extended_tweet"]["full_text"])
+                body = "{0}: {1}".format(
+                    tweet["text"].split(":")[0],
+                    tweet["retweeted_status"]["extended_tweet"]["full_text"]
+                )
             else:
                 body = tweet["text"]
         else:
@@ -45,6 +49,7 @@ async def recv_tweets(websocket):
         pending_tweets[tweet["id"]] = tweet_info
         await websocket.send(json.dumps(tweet_info))
 
+
 async def recv_decisions(websocket):
     async for message in websocket:
         data = json.loads(message)
@@ -54,6 +59,7 @@ async def recv_decisions(websocket):
         elif data["decision"] == "REJECT":
             del pending_tweets[message["id"]]
 
+
 async def ws_tweets(websocket, path):
     print("Websocket Connected")
     try:
@@ -61,15 +67,17 @@ async def ws_tweets(websocket, path):
         await asyncio.gather(
             recv_tweets(websocket),
             recv_decisions(websocket)
-            )
+        )
     except websockets.exceptions.ConnectionClosedError:
         print("RIP Connection")
+
 
 def ws_server() -> None:
     print("Starting WebSocket Server")
     ws = websockets.serve(ws_tweets, config.HOST, config.WS_PORT)
     asyncio.get_event_loop().run_until_complete(ws)
     asyncio.get_event_loop().run_forever()
+
 
 if __name__ == "__main__":
     print("Don't do this")
