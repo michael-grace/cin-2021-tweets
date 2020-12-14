b9a4235ae1096ce4475d5ca17f989340bad8528f
Colin <roittcolin@gmail.com>
2020-12-14 21:28:22 +0000

    use bloody double quotes....

--- static/board.js
+++ static/board.js
@@ -21,10 +21,10 @@ xhttp.onreadystatechange = function() {
         ws.onmessage = function(event) {
             tweetJson = JSON.parse(event.data);
             tweetHtml = JSON.parse(tweetJson.html);
-            let newTweet = document.createElement('div');
+            let newTweet = document.createElement("div");
             newTweet.innerHTML = tweetHtml.html;
             newTweet.classList = "w-25 p-3";
-            document.querySelector('tweets').prepend(newTweet);
+            document.querySelector("tweets").prepend(newTweet);
             twttr.widgets.load(newTweet);
         };
         ws.onclose = function() {
