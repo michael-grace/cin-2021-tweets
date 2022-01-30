2d87c86a2f774fd04697d687b3489e3847500a13
michael-grace <michael.dp.grace@outlook.com>
2022-01-30 20:23:03 +0000

    fixing envrionment variables

--- pkg/twitter/tweets.go
+++ pkg/twitter/tweets.go
@@ -8,14 +8,31 @@ package twitter
 
 import (
 	"os"
+	"strings"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
 func GetTweetStream(tweets chan<- *twitter.Tweet) {
-	config := oauth1.NewConfig(os.Getenv("TWITTER_CONSUMER_KEY"), os.Getenv("TWITTER_CONSUMER_SECRET"))
-	token := oauth1.NewToken(os.Getenv("TWITTER_OAUTH_TOKEN"), os.Getenv("TWITTER_OAUTH_SECRET"))
+
+	/**
+	I just want to let the world know, I spent absolutely
+	ages trying to work out why it was fine when I gave the
+	strings to it directly but didn't work when I passed them
+	through environment variables. It took for absolutely ever,
+	and it just turns out they had carriage returns at the end
+	of them.
+	*/
+
+	consumerKey := strings.TrimSuffix(os.Getenv("TWITTER_CONSUMER_KEY"), "\r")
+	consumerSecret := strings.TrimSuffix(os.Getenv("TWITTER_CONSUMER_SECRET"), "\r")
+	oauthToken := strings.TrimSuffix(os.Getenv("TWITTER_OAUTH_TOKEN"), "\r")
+	oauthSecret := strings.TrimSuffix(os.Getenv("TWITTER_OAUTH_SECRET"), "\r")
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(oauthToken, oauthSecret)
+
 	httpClient := config.Client(oauth1.NoContext, token)
 
 	client := twitter.NewClient(httpClient)
