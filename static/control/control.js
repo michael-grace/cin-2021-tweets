5e53caa4c56d898c0ae532e689f27197fc5a67f2
michael-grace <michael.dp.grace@outlook.com>
2022-02-14 21:09:16 +0000

    license stuff

--- static/control/control.js
+++ static/control/control.js
@@ -1,12 +1,20 @@
 /**
-    URY Tweet Board
-    Candidate Interview Night 2021
+URY Tweet Board
+Copyright (C) 2020, 2022 Michael Grace <michael.grace@ury.org.uk>
 
-    Author: Michael Grace
-    Date: November 2020, January 2022
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    github.com/UniversityRadioYork
- */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 let scheme = window.location.protocol === "https:" ? "wss://" : "ws://"
 
